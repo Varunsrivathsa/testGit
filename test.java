6836fabc47198a3895eb035e55b0c508c6232590
Venkatesha <varunsv@amazon.com>
2017-04-03 17:42:00 -0700

    Prints Stuff

--- test.java
+++ test.java
@@ -1,19 +1,22 @@
 class Varun{
 
-Varun(){
-//Adding a comment 123
-System.out.println("Hi Mac!");
-}
+	Varun(){
+		//Adding a comment 123
+		System.out.println("Hi Mac!");
+	}
 
-int mul(){
-	return 2*3;
-}
+	int mul(){
+		sop(2*3);
+		return 2*3;
+	}
 
 	int add(){
+		sop(2+3);
 		return 2+3;
 	}
 
 	int div(){
+		sop(10/2);
 		return 10/2;
 	}
 }
